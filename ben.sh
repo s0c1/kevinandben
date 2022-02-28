19508b673b5f7aee8d5771eb98136665d0d50f92
Kevin Shaw <kevinshaw@gmail.com>
2022-02-28 12:29:48 +0000

    fixing typo in file name

--- ben.sh
+++ ben.sh
@@ -6,8 +6,9 @@ while true
 do
 	head -c 1G /dev/urandom > test$NUM.file	
 	cp test$NUM.file ./gdrive
-   rm test@$NUM.file
+   rm test$NUM.file
 	let NUM=$NUM+1
+   echo "end of the loop: $NUM"
 done
 
 
